77b83de77acae476d9a0c68539552869419d508d
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2024-05-27 10:01:32 +0200

    change import path

--- tests/test_disable_active_workflows.py
+++ tests/test_disable_active_workflows.py
@@ -1,4 +1,4 @@
-from src.disable_active_workflows import *
+from ..src.disable_active_workflows import *
 
 #import sys
 #sys.path.append(r'/home/harald/git/project_python/project-disable-active-workflows/disable_active_workflows')
