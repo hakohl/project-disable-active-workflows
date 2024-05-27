feb43343a33e9564bc69092eb5f6389e73aeb180
Harald Kohlmann <Harald.Kohlmann@gmx.de>
2024-05-27 08:25:17 +0200

    change import again

--- src/__init__.py
+++ src/__init__.py
@@ -2,4 +2,4 @@ print(f'Invoking __init__.py for {__name__}')
 
 #import disable_active_workflows.disable_active_workflows
 
-__all__ = ['disable_active_workflows']
+#__all__ = ['disable_active_workflows']
